1f03cb8ec2b553f3a2d2dd29d4f9181153a110e3
Nabeel Ahmed <nabeelrajpoot314@gmail.com>
2020-09-07 15:26:11 +0500

    first

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float runSpeed = 40f;
+
+    public float horizontalMove;
+    public float verticalMove;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(horizontalMove, verticalMove) * runSpeed;
+    }
+
+}
---
+++ Assets/Scripts/ScreenBounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBounds : MonoBehaviour
+{
+    private Vector3 screenBounds;
+    private Vector3 screenSize;
+    public GameObject player;
+    private float objectWidth;
+    private float objectHeight;
+    private void Start()
+    {
+        screenSize = new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z);
+        screenBounds = Camera.main.ScreenToWorldPoint(screenSize);
+        objectWidth = player.transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        objectHeight = player.transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        player.transform.position = new Vector2(
+            Mathf.Clamp(player.transform.position.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth),
+            Mathf.Clamp(player.transform.position.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight));
+    }
+}
